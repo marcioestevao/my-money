2b0a82fe2242e5b0d29cad3fda0e97d6cc61b33e
Marcio Estevão Martins <p017328@rede.sp>
2018-07-31 15:12:41 -0300

    Alterado a versão do mongoose para funcionamento da função aggregate. Ou fazia update do mongoose ou downgrade do mongodb. optei pelo upgrade do mongoose. (http://mongoosejs.com/docs/compatibility.html)

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
-
+module.exports = mongoose.connect('mongodb://localhost/mymoney', {
+   useMongoClient: true 
+})
 
 mongoose.Error.messages.general.required = "O Atributo '{PATH}' é obrigatório"
 mongoose.Error.messages.Number.min = 
